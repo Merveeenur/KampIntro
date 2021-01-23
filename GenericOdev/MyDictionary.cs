f40d56a7bb100841aa6b9dc29dad47e1ccd2c521
mervenur teke <313958@ogr.ktu.edu.tr>
2021-01-23 19:25:52 +0300

    MyDİctionaryOdev

---
+++ GenericOdev/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericOdev
+{
+    class MyDictionary<T, T1>
+    {
+        T[] itemId;
+        T1[] itemName;
+        public MyDictionary()
+        {
+            itemId = new T[0];
+            itemName = new T1[0];
+        }
+        
+        public void Add(T ıd,T1 name)
+        {
+            T[] tempArray = itemId;
+            T1[] tempArray1 = itemName;
+
+
+            itemId = new T[itemId.Length + 1];
+            itemName = new T1[itemName.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                itemId[i] = tempArray[i];
+                itemName[i] = tempArray1[i];
+            }
+            itemId[itemId.Length - 1] = ıd;
+            itemName[itemName.Length - 1] = name;
+            
+        }
+        public int Count
+        {
+            get { return itemId.Length; }
+        }
+        public T1[] Items
+        {
+            get { return itemName; }
+        }
+    }
+}
---
+++ GenericOdev/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GenericOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyDictionary<int,string> items = new MyDictionary<int,string>();
+            items.Add(3, "merve");
+            items.Add(4, "emir");
+
+
+            Console.WriteLine(items.Count);
+
+            items.Add(5, "cengiz");
+            items.Add(6, "meltem");
+           
+            foreach (var liste in items.Items)
+            {
+                Console.WriteLine(liste);
+            }
+            Console.WriteLine(items.Count);
+
+
+        }
+    }
+}
+ 
