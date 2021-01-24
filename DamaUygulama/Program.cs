1156f43442ac1587e3c583d7e31423213d7a7bd1
mervenur teke <313958@ogr.ktu.edu.tr>
2021-01-24 17:36:32 +0300

    dama

---
+++ DamaUygulama/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DamaUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+        }
+        class CustomerManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Customer Added");
+            }
+            public void Update()
+            {
+                Console.WriteLine("Customer Update");
+            }
+
+        }
+        class ProductManager
+        {
+            public void Add(string name)
+            {
+                Console.WriteLine("Product Added");
+            }
+            public void Update()
+            {
+                Console.WriteLine("Product Update");
+            }
+        }
+    }
+}
